41427ab8dba16b0a8c7f8ea3ca4527f3eabf2d6c
Pawel <pawmag.code@gmail.com>
2024-11-23 20:57:27 +0100

    Create Database module use dagger-hilt for dependency injection setup

---
+++ app/src/main/java/com/wsb/my2do/di/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.wsb.my2do.di
+
+import android.content.Context
+import androidx.room.Room
+import com.wsb.my2do.data.ToDoDatabase.ToDoDatabase
+import com.wsb.my2do.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): ToDoDatabase = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+}
