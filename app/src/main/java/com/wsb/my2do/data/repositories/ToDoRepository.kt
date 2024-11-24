f665e44286227a07a692e70b3c3ca5411a59308f
Pawel <pawmag.code@gmail.com>
2024-11-24 19:32:58 +0100

    Create Shared view model,  inject tot repository, create one finction to fetch all tasks from database table

--- app/src/main/java/com/wsb/my2do/data/repositories/ToDoRepository.kt
+++ app/src/main/java/com/wsb/my2do/data/repositories/ToDoRepository.kt
@@ -3,9 +3,11 @@ package com.wsb.my2do.data.repositories
 import androidx.room.Query
 import com.wsb.my2do.data.ToDoDao
 import com.wsb.my2do.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao ) {
 
     val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
